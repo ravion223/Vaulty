ce25f3254548ec1862a93c618448ad451500a931
Rostyslav <tablettaoson2016@gmail.com>
2026-06-03 19:35:12 +0300

    security: update CORS for production Vercel frontend

--- vaulty/settings.py
+++ vaulty/settings.py
@@ -145,7 +145,8 @@ STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://vaulty-kappa.vercel.app"
 ]
 
 SIMPLE_JWT = {
